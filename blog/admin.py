2555115a4da209ecd55db81a71648f7b3e71a9f4
Philipp <philipp_w_94@hotmail.de>
2020-06-10 11:42:32 +0200

    abc

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,4 @@
-'Hi Leute'
 from django.contrib import admin
 from .models import Post
 
-<<<<<<< HEAD
 admin.site.register(Post)
-#Test von git - Linea
-
-=======
-admin.site.register(Post)
->>>>>>> 457422c1d11fb5de1acac1411ecfa09ab877f28a
